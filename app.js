d4a8886263571c0cd29a4ca2d21ac048ee8c891b
mhashirb <hashirbaig275@gmail.com>
2024-03-15 18:20:17 -0700

    Assignment 20

---
+++ app.js
@@ -0,0 +1,126 @@
+
+//Chapter 17-20
+// Question 1
+// var a=10;
+// for(var i=0;i<a;i++){
+//     document.write("Loop");
+// }
+
+// Question 2
+// for(var usual=0;usual<12;usual++){
+//     document.write("loop");
+// }
+
+// Question 3
+// for(var i = 0;i <= 4;i++){}
+
+// Question 4
+// for(var j=0;j<100;j++){
+//     document.write("<br>loop");
+// }
+
+// Question 5
+// var j=4;
+// for(var i=7;i>j;i--){
+//     document.write("Loop");
+// }
+
+// Question 6
+// var arr=["one","two","three","four"];
+// var j=0;
+// for(var i=0;i<arr.length();i++){
+//     j++;
+// }
+// alert("The number of elements in an array are"+j);
+
+// // Question 7
+// var flag=true;
+
+// Question 8
+// var pets=["parrot","dog","cat","rabbit"];
+// for(var i=0;i<pets.length;i++){
+//         document.write(pets[i]);
+// }
+
+// Question 9
+// var a=1;
+// for(var i=0;i<10;i++){
+//     a++;
+//     break;
+// }
+// alert(a);
+
+// Question 10
+// var arr=["ali","hashir","zain","junaid"];
+// var nameFound="no";
+// var userName=prompt("Enter user name");
+// userName=userName.toLowerCase();
+// for(var i=0;i<arr.length;i++){
+//     if(userName===arr[i]){
+//         nameFound="yes";
+//         break;
+//     }
+// }
+// if(nameFound==="yes"){
+//     alert("Enter");
+// }
+// else{
+//     alert("please enter correct user name");
+// }
+
+// // Question 11
+// var arr=["ali","hashir","zain","junaid"];
+// var nameFound=false;
+// var userName=prompt("Enter user name");
+// userName=userName.toLowerCase();
+// for(var i=0;i<arr.length;i++){
+//     if(userName===arr[i]){
+//         matchFound=true;
+//         break;
+//     }
+// }
+// if(nameFound==="true"){
+//     alert("Enter");
+// }
+// else{
+//     alert("please enter correct user name");
+// }
+
+// Question 12
+// var firstArr=["a","b","c","d","e","f"];
+// var secondArr=["1","2","3","4","5","6"];
+// var mixedArr=[];
+// for(var i=0;i<firstArr.length;i++){
+//     for(var j=0;j<secondArr.length;j++){
+//         mixedArr.push(firstArr[i]+secondArr[i]);
+//     }
+// }
+//     alert(mixedArr);
+
+//Some Pdf questions
+// Question 1
+// var arr=[[],[],[],[]];
+
+// Question 2
+// for(var i=0)
+
+//Question 3
+// var a=1;
+// for(var i=0;i<10;i++){
+//     document.write("<br>"+a);
+//     a++;
+// }
+
+//Question 4
+// var number=+prompt("Enter a number to struct table");
+// var length=+prompt("Enter the length of table");
+// for(var i=1;i<=length;i++){
+//     document.write("<br>"+number+" x "+i+" = "+number*i);
+// }
+
+// Question 5
+// var fruits=["apple","banana","mango","strawberry"];
+// for(var i=0;i<fruits.length;i++){
+//     document.write("<br>Element at index "+(i+1)+" is "+fruits[i]);
+// }
+
